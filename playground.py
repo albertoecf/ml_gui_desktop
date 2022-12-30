b8e3ce05753cc9d8b1541c9ade2cf5218f41e8df
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-30 15:57:11 +0100

    feat: creating a window with text and title

--- playground.py
+++ playground.py
@@ -1,3 +1,18 @@
 #%%
 print('im working')
+
 #%%
+from tkinter import * 
+# %%
+window = Tk()
+window.title('Testing Window')
+
+# Add text into a window :
+# 1 create text : 
+welcome_message = Label(window, text= "Welcome to our first window")
+# 2 add text into window
+
+window.mainloop()
+# %%
+
+

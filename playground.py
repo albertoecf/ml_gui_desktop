1822ed82e0a8e18003be2d3779e2c785804a2e8d
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-30 16:39:46 +0100

    feat: def naive function

--- playground.py
+++ playground.py
@@ -3,20 +3,31 @@ print('im working')
 
 #%%
 from tkinter import * 
+#%%
+#defining functions : 
+
+def print_date():
+    from datetime import date
+    today = date.today()
+    print("Today's date:", today)
+
 # %%
 window = Tk()
-window.title('Testing Window')
+window.title('Morning Window')
 
 # Add text into a window :
 # 1 create text : 
-welcome_message = Label(window, text= "Welcome to our first window")
+welcome_message = Label(window, text= "Let's have a great day")
 # 2 add text into window
 welcome_message.pack()
-# Configure window size : 
-window.geometry("500x50")
+#(optional) Configure window size : 
+window.geometry("500x100")
 
-#Add a button to quit execution
+#(optional) Add a button to quit execution
 Button(window, text="Quit", command=window.quit).pack()
+Button(window, text="Date", command=print_date).pack()
+
+# run our window
 window.mainloop()
 # %%
 

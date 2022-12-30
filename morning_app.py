42173bea7efecffa918e79861191a0e5bb81b5d3
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-30 16:22:49 +0100

    feat: showing today's date

--- morning_app.py
+++ morning_app.py
@@ -1,6 +1,14 @@
 #%%
 print('d')
 #%%
+def show_date():
+    from datetime import date
+    today = date.today()
+    today_text = str(today.strftime("%b-%d-%Y"))
+    today_text = "Today's date: {date_text} ".format(date_text = today_text)
+    welcome_message.config(text = today_text)
+    print(today_text)
+#%%
 from tkinter import *
 # %%
 window = Tk()
@@ -8,3 +16,16 @@ window.title('Morning App')
 
 welcome_message = Label(window, text="Today is going to be awesome")
 welcome_message.pack() # we could pack in the same line
+
+
+
+window.geometry("500x100")
+
+#(optional) Add a button to quit execution
+Button(window, text="Quit", command=window.quit).pack()
+Button(window, text="Date", command=show_date).pack()
+
+# run our window
+window.mainloop()
+
+# %%

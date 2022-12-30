464fbc90c72dafcb07d423195ece871b8600ec19
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-30 16:14:07 +0100

    feat: start morning app

---
+++ morning_app.py
@@ -0,0 +1,10 @@
+#%%
+print('d')
+#%%
+from tkinter import *
+# %%
+window = Tk()
+window.title('Morning App')
+
+welcome_message = Label(window, text="Today is going to be awesome")
+welcome_message.pack() # we could pack in the same line

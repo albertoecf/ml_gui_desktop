0f6267736a0d01e6148c26e4a0511e041eb776d3
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-31 11:19:03 +0100

    feat: working gui classify images

--- gui_classify_images.py
+++ gui_classify_images.py
@@ -21,21 +21,33 @@ def upload_file():
 
 def predict_image():
     global uploaded_image
+    
     file_path = filedialog.askopenfilename()
     uploaded_image = ImageTk.PhotoImage(file=file_path)
+    image_label = Label(input_window)
+    image_label.image = uploaded_image
+    image_label['image'] = uploaded_image
+    image_label.pack()
+    
     loaded_model = load_model('burger_vs_boxex_CNN.h5')
     img_loaded = Image.open(file_path)
     img_resized = Image.Image.resize(img_loaded, (100,100))
     img = (np.array(img_resized)-127.5)/127.5
     img = img.reshape(1,100,100,3)
+    
     prediction = loaded_model.predict(img) 
     classes_x=np.argmax(prediction,axis=1)
+    
     if classes_x == 0:
-        print("Prediction : burger")
+        l2.config(
+        text= "Product Prediction : Burger",fg="white", bg="green")
     elif classes_x == 1 :
-        print('Prediction : Box')
+        l2.config(
+        text= "Product Prediction : Box",fg="white" ,bg="green")
     else : 
-        print('could not decide')
+        l2.config(
+        text= "Couldn't predict. Please try again or other image")
+
 
 
 
@@ -43,12 +55,15 @@ def predict_image():
 input_window = Tk()
 input_window.title('Classify Image')
 l1 = Label(input_window,text='Upload your image').pack()
-
-
-b1 = Button(input_window, text='Upload File', 
+l2 = Label(input_window, text= "Waiting user input", bg='grey')
+l2.pack()
+b0 = Button(input_window, text='Show File', 
+   width=20,command = lambda:upload_file()).pack()
+b1 = Button(input_window, text='Predict Class', 
    width=20,command = lambda:predict_image()).pack()
 
 Button(input_window, text="Quit",
        command=input_window.quit).pack()
 input_window.mainloop()
+
 # %%
